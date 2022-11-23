39e80a8573cc33261267ba480c9ae6fa7bc07011
KarthyV <Kdon979@gmail.com>
2022-11-23 11:22:58 +0530

    navbar comp - init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="header">
-      <h3>Kanban</h3>
+      <div className="title">
+        <img
+          src="https://kanban-app-jay.netlify.app/assets/logo-light.4fb0dd87.svg"
+          alt="logo"
+        />
+      </div>
+      <div className="navbar_content">
+        <h2>Platform Launch</h2>
+        <button className="add_Btn_header">+ Add New task</button>
+      </div>
     </div>
   );
 };
